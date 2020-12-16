80b2c6a0d12f7bd6f681b42ef3bca8f180f6036d
saag67 <Anoshkin1@yandex.ru>
2020-12-16 20:05:44 +0500

    Added first screen

--- places/lib/main.dart
+++ places/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(App());
@@ -9,9 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Fundamential",
-      home: MyFirstWidget(
-          //title: "Fundamential",      //свойство title для Stateful виджета MyHomePage
-          ),
+      home: SightListScreen(),
     );
   }
 }
